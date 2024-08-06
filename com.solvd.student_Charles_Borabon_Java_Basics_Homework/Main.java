aa09bda0bcf206c61e904237635327a67e897230
Z_ <zxcvbbvvf2@gmail.com>
2024-08-06 11:06:31 -0500

    fixed main.java

---
+++ com.solvd.student_Charles_Borabon_Java_Basics_Homework/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // Simple print statement using variables 
+        String firstName = "Charles";
+        String lastName = "Borabon";
+        int age = 23;
+        
+        System.out.println("Hello, my name is " + firstName + " " + lastName + " and I am " + age + " years old.");
+    }
+}
