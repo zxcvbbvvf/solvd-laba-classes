f4a5c5ca2ac68b063d47c5d624edb66a650896e0
Z_ <zxcvbbvvf2@gmail.com>
2024-08-06 11:22:27 -0500

    updated main class

--- com.solvd.student_Charles_Borabon_Java_Basics_Homework/Main.java
+++ com.solvd.student_Charles_Borabon_Java_Basics_Homework/Main.java
@@ -7,5 +7,22 @@ public class Main {
         int age = 23;
         
         System.out.println("Hello, my name is " + firstName + " " + lastName + " and I am " + age + " years old.");
+        System.out.println();
+
+        // Quicksort test
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        System.out.println("Before sorting: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        Quicksort quicksort = new Quicksort();
+
+        quicksort.sort(arr, 0, arr.length - 1);
+        // Print sorted array
+        System.out.println("\nAfter sorting: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
