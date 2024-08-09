2a03df616a929ddb1b65a44918219b6a1ef837d2
Z_ <zxcvbbvvf2@gmail.com>
2024-08-09 08:40:43 -0500

    updated main

--- com.solvd.student_Charles_Borabon_OOP_Approach/Main.java
+++ com.solvd.student_Charles_Borabon_OOP_Approach/Main.java
@@ -1,5 +1,22 @@
+import bank.Bank;
+import bank.BankCustomer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // bank package test
+        Bank bank = new Bank(123456, "John Doe", 1000.00);
+        bank.display();
+        bank.deposit(500.00);
+        bank.display();
+        bank.withdraw(200.00);
+        bank.display();
+        System.out.println();
+
+        BankCustomer customer = new BankCustomer(123456, "John Doe", 1000.00, "Jane Doe", "1234 Elm St", "555-555-5555", "jane@email.com");
+        customer.display();
+        customer.deposit(500.00);
+        customer.display();
+        customer.withdraw(200.00);
+        customer.display();
     }
 }
