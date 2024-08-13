3397b823b6c8008eb447e941f9a67b89079d49b3
Z_ <zxcvbbvvf2@gmail.com>
2024-08-13 10:51:27 -0500

    updated bank package to implement interface

--- com.solvd.student_Charles_Borabon_OOP_Approach_Part_2/bank/BankCustomer.java
+++ com.solvd.student_Charles_Borabon_OOP_Approach_Part_2/bank/BankCustomer.java
@@ -1,25 +1,61 @@
 package bank;
 
-public class BankCustomer extends Bank {
-    String customerName;
-    String customerAddress;
-    String customerPhone;
-    String customerEmail;
+public final class BankCustomer extends Bank implements BankCustomerMethods {
+    private final int accountNumber;
+    private double balance;
+    private final String customerName;
+    private final String customerAddress;
+    private final String customerPhone;
+    private final String customerEmail;
+    static int customerCount = 0;
+
+    public static int getCustomerCount() {
+        return customerCount;
+    }
 
     public BankCustomer(int accountNumber, String accountHolder, double balance, String customerName, String customerAddress, String customerPhone, String customerEmail) {
-        super(accountNumber, accountHolder, balance);
+        this.accountNumber = accountNumber;
+        this.balance = balance;
         this.customerName = customerName;
         this.customerAddress = customerAddress;
         this.customerPhone = customerPhone;
         this.customerEmail = customerEmail;
+        customerCount++;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (this.balance - amount < 0) {
+            System.out.println("Insufficient funds.");
+        } else {
+            this.balance -= amount;
+        }
+    }
+
+    @Override
+    public int getAccountNum() {
+        return this.accountNumber;
+    }
+
+    @Override
+    public String getAccountName() {
+        return this.customerName;
+    }
+
+    @Override
+    public int getBalance() {
+        return (int) this.balance;
     }
 
     @Override
     public String toString() {
-        return "Customer Name: " + this.customerName + "\n"
-             + "Customer Address: " + this.customerAddress + "\n"
-             + "Customer Phone: " + this.customerPhone + "\n"
-             + "Customer Email: " + this.customerEmail + "\n"
-             + "Current Balance: " + this.balance + "\n";
+        return "Customer ID" + this.getAccountNum() + "\n"
+             + "Customer Name: " + this.getAccountName() + "\n"
+             + "Customer Balance: " + this.getBalance() + "\n";
     }
 }
---
+++ com.solvd.student_Charles_Borabon_OOP_Approach_Part_2/bank/BankCustomerMethods.java
@@ -0,0 +1,7 @@
+package bank;
+
+public interface BankCustomerMethods {
+    public int getAccountNum();
+    public String getAccountName();
+    public int getBalance();
+}
