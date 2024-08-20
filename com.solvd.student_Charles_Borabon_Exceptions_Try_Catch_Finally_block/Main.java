5e983517dff8b7499a9b615f3224099473530e94
Z_ <zxcvbbvvf2@gmail.com>
2024-08-20 10:06:55 -0500

    removed abstract and added necessary jar files

--- com.solvd.student_Charles_Borabon_Exceptions_Try_Catch_Finally_block/Main.java
+++ com.solvd.student_Charles_Borabon_Exceptions_Try_Catch_Finally_block/Main.java
@@ -1,5 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The Main class will test try and catch blocks with loggers
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        try {
+            int[] arr = new int[5];
+            arr[6] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.error("Array index is out of bounds");
+        }
     }
 }
