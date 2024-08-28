f90a3ec5d37844848576984a4f511817fe58afd3
Z_ <zxcvbbvvf2@gmail.com>
2024-08-28 08:30:56 -0500

    created main and initialized 4 different Collections

---
+++ com.solvd.student_Charles_Borabon_Collections_and_Generics/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize five different Collectoin objects
+        List<String> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Queue<Integer> queue = new PriorityQueue<>();
+        Map<String, Integer> map = new HashMap<>();
+    }
