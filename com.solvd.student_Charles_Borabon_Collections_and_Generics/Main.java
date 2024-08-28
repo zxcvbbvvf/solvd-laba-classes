01e5cfb832856e7989983f12fca983000f9b9d40
Z_ <zxcvbbvvf2@gmail.com>
2024-08-28 08:56:43 -0500

    Implemented test cases for Collections in Main

--- com.solvd.student_Charles_Borabon_Collections_and_Generics/Main.java
+++ com.solvd.student_Charles_Borabon_Collections_and_Generics/Main.java
@@ -1,11 +1,11 @@
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.PriorityQueue;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,4 +15,46 @@ public class Main {
         Queue<Integer> queue = new PriorityQueue<>();
         Map<String, Integer> map = new HashMap<>();
         CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+
+        // Add elements to list and print
+        list.add("Hello");
+        list.add("World");
+        System.out.println("List: " + list);
+        System.out.println("--------------------");
+
+        // Add elements to set and print
+        set.add(4);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        System.out.println("Set: " + set);
+        System.out.println("--------------------");
+
+        // Add elements to queue and print
+        queue.add(4);
+        queue.add(2);
+        queue.add(3);
+        queue.add(1);
+        System.out.println("Queue: " + queue);
+        // Remove one element from queue and print
+        queue.poll();
+        System.out.println("Queue: " + queue);
+        System.out.println("--------------------");
+
+        // Add elements to map and print
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+        System.out.println("Map: " + map);
+        System.out.println("--------------------");
+
+        // Add elements to linked list and print
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        System.out.println("Linked List: " + linkedList);
+        // Show current head and tail
+        System.out.println("Head: " + linkedList.getHead());
+        System.out.println("Tail: " + linkedList.getTail());
     }
+}
