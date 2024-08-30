77f162792df191a38b8eb5a14ae79cb9423f8067
Z_ <zxcvbbvvf2@gmail.com>
2024-08-30 11:08:48 -0500

    moved to maven

---
+++ com.solvd.student_Charles_Borabon_OOP_Approach_Maven/src/main/java/org/CharlesBorabon/OOP/Main.java
@@ -0,0 +1,48 @@
+package org.CharlesBorabon.OOP;
+
+import org.CharlesBorabon.OOP.bank.Bank;
+import org.CharlesBorabon.OOP.bank.BankCustomer;
+import org.CharlesBorabon.OOP.management.Employee;
+import org.CharlesBorabon.OOP.management.Management;
+import org.CharlesBorabon.OOP.zoo.Zoo;
+import org.CharlesBorabon.OOP.zoo.ZooAnimal;
+
+public class Main {
+    public static void main(String[] args) {
+        // bank package test
+        Bank bank = new Bank(123456, "John Doe", 1000.00);
+        System.out.println(bank.toString());
+        System.out.println("--------------------");
+
+        BankCustomer customer = new BankCustomer(123456, "John Doe", 1000.00, "Jane Doe", "1234 Elm St", "555-555-5555", "jane@email.com");
+        System.out.println(customer.toString());
+        customer.deposit(500.00);
+        System.out.println(customer.toString());
+        customer.withdraw(200.00);
+        System.out.println(customer.toString());
+        System.out.println("--------------------");
+
+        // zoo package test
+        Zoo zoo = new Zoo(1, "Lion");
+        System.out.println(zoo.toString());
+        System.out.println("--------------------");
+
+        ZooAnimal zooAnimal = new ZooAnimal(1, "Lion", "Roar");
+        ZooAnimal zooAnimal2 = new ZooAnimal(1, "Lion", "Roar");
+        System.out.println(zooAnimal.toString());
+        System.out.println("Are the two ZooAnimal obejcts the same? " + zooAnimal.equals(zooAnimal2));
+        System.out.println("Hash code for zooAnimal: " + zooAnimal.hashCode());
+        System.out.println("Hash code for zooAnimal2: " + zooAnimal2.hashCode());
+        System.out.println("--------------------");
+
+        // management package test
+        Management management = new Management("John Doe");
+        System.out.println(management.toString());
+        System.out.println("--------------------");
+
+        Employee employee = new Employee("John Doe", "HR", "Manager");
+        Employee employee2 = new Employee("Jane Doe", "HR", "Manager");
+        System.out.println(employee.toString());
+        System.out.println(employee2.toString());
+    }
+}
