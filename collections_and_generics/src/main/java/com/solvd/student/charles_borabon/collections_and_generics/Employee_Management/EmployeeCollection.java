030d00402467a6166e9ff0ce3798cce609533948
Z_ <zxcvbbvvf2@gmail.com>
2024-09-13 11:50:10 -0500

    added EmployeeCollection class

---
+++ collections_and_generics/src/main/java/com/solvd/student/charles_borabon/collections_and_generics/Employee_Management/EmployeeCollection.java
@@ -0,0 +1,26 @@
+package com.solvd.student.charles_borabon.collections_and_generics.Employee_Management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeCollection {
+    protected List<Employee> employees;
+
+    public EmployeeCollection() {
+        employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        employees.remove(employee);
+    }
+
+    public void listEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+}
