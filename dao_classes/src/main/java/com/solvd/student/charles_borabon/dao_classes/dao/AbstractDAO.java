0870749f28a444ca3da6021398e01983df1969a5
Z_ <zxcvbbvvf2@gmail.com>
2024-10-09 10:07:14 -0500

    created AbstractDAO class

---
+++ dao_classes/src/main/java/com/solvd/student/charles_borabon/dao_classes/dao/AbstractDAO.java
@@ -0,0 +1,22 @@
+package com.solvd.student.charles_borabon.dao_classes.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class AbstractDAO<T, ID> implements GenericDAO<T, ID> {
+    protected ConnectionPool connectionPool;
+
+    public AbstractDAO() {
+        this.connectionPool = ConnectionPool.getInstance();
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return connectionPool.getConnection();
+    }
+
+    protected void closeConnection(Connection connection) {
+        connectionPool.releaseConnection(connection);
+    }
+
+    // CRUD operations will be partially implemented or left for concrete classes
+}
