bff232b8ed1323798c92ef15e06c2b95532a624d
Z_ <zxcvbbvvf2@gmail.com>
2024-10-09 10:24:03 -0500

    created hotel class to interact with database

---
+++ dao_classes/src/main/java/com/solvd/student/charles_borabon/dao_classes/dao/Hotel.java
@@ -0,0 +1,68 @@
+package com.solvd.student.charles_borabon.dao_classes.dao;
+
+public class Hotel {
+    private int hotelId;
+    private String hotelName;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String phone;
+
+    // Getters and Setters
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
