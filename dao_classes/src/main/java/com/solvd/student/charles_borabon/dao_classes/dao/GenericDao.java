d8b6b05a42def7299b7edf7ce2aa0c5cb69f16ce
Z_ <zxcvbbvvf2@gmail.com>
2024-10-09 08:28:30 -0500

    created GenericDao interface

---
+++ dao_classes/src/main/java/com/solvd/student/charles_borabon/dao_classes/dao/GenericDao.java
@@ -0,0 +1,17 @@
+package com.solvd.student.charles_borabon.dao_classes.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericDao<T, ID> {
+    Optional<T> findById(ID id) throws SQLException;
+
+    List<T> findAll() throws SQLException;
+
+    boolean save(T entity) throws SQLException;
+
+    boolean update(T entity) throws SQLException;
+
+    boolean deleteById(ID id) throws SQLException;
+}
