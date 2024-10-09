35100bbd02afbbdcf5e906059edbe99f987cd10d
Z_ <zxcvbbvvf2@gmail.com>
2024-10-09 09:10:07 -0500

    updated GenericDao class

--- dao_classes/src/main/java/com/solvd/student/charles_borabon/dao_classes/dao/GenericDao.java
+++ dao_classes/src/main/java/com/solvd/student/charles_borabon/dao_classes/dao/GenericDao.java
@@ -2,16 +2,11 @@ package com.solvd.student.charles_borabon.dao_classes.dao;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 
 public interface GenericDAO<T, ID> {
-    Optional<T> findById(ID id) throws SQLException;
-
-    List<T> findAll() throws SQLException;
-
-    boolean save(T entity) throws SQLException;
-
-    boolean update(T entity) throws SQLException;
-
-    boolean deleteById(ID id) throws SQLException;
+    void create(T entity) throws SQLException;
+    T read(ID id) throws SQLException;
+    void update(T entity) throws SQLException;
+    void delete(ID id) throws SQLException;
+    List<T> getAll() throws SQLException;
 }
