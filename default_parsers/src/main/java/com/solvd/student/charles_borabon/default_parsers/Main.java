fabab9c74de11f2abbeb42a4031106bb9638754c
Z_ <zxcvbbvvf2@gmail.com>
2024-10-16 08:22:02 -0500

    created a new project

---
+++ default_parsers/src/main/java/com/solvd/student/charles_borabon/default_parsers/Main.java
@@ -0,0 +1,8 @@
+package com.solvd.student.charles_borabon.default_parsers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
