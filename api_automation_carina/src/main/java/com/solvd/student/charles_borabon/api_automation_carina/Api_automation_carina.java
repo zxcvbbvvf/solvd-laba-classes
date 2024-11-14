74291a5ce0a208c4eb6afacfd48334ea23fe2f8d
Z_ <zxcvbbvvf2@gmail.com>
2024-11-14 08:35:08 -0600

    created new project

---
+++ api_automation_carina/src/main/java/com/solvd/student/charles_borabon/api_automation_carina/Api_automation_carina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.solvd.student.charles_borabon.api_automation_carina;
+
+/**
+ *
+ * @author Dustin_2
+ */
+public class Api_automation_carina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
