fc2953386896f1cd0addf2613d091fd0e1d190af
Z_ <zxcvbbvvf2@gmail.com>
2024-11-16 09:00:19 -0600

    implemented UpdateRepoMethod class

--- api_automation_carina/src/test/java/com/example/api/UpdateRepoMethod.java
+++ api_automation_carina/src/test/java/com/example/api/UpdateRepoMethod.java
@@ -1,5 +1,12 @@
 package com.example.api;
 
-public class UpdateRepoMethod {
-    
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.utils.config.Configuration;
+
+public class UpdateRepoMethod extends AbstractApiMethodV2 {
+    public UpdateRepoMethod(String owner, String repo) {
+        super("api/update_repo.json", null);
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        setMethodPath("/repos/" + owner + "/" + repo);
+    }
 }
