130d51998672dad5fa8e3e60c32113ebcf643d9d
Z_ <zxcvbbvvf2@gmail.com>
2024-11-16 09:03:41 -0600

    implemented GetReposListMethod class

--- api_automation_carina/src/test/java/com/example/api/GetReposListMethod.java
+++ api_automation_carina/src/test/java/com/example/api/GetReposListMethod.java
@@ -1,5 +1,12 @@
 package com.example.api;
 
-public class GetReposListMethod {
-    
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.utils.config.Configuration;
+
+public class GetReposListMethod extends AbstractApiMethodV2 {
+    public GetReposListMethod(String owner) {
+        super(null, null);
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        setMethodPath("/users/" + owner + "/repos");
+    }
 }
