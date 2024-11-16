03756d00f946f94dd64c1fa31de771edaeaf82f8
Z_ <zxcvbbvvf2@gmail.com>
2024-11-16 08:58:29 -0600

    implemented GetRepoMethod class

--- api_automation_carina/src/test/java/com/example/api/GetRepoMethod.java
+++ api_automation_carina/src/test/java/com/example/api/GetRepoMethod.java
@@ -1,5 +1,12 @@
 package com.example.api;
 
-public class GetRepoMethod {
-    
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.utils.config.Configuration;
+
+public class GetRepoMethod extends AbstractApiMethodV2 {
+    public GetRepoMethod(String owner, String repo) {
+        super(null, null);
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        setMethodPath("/repos/" + owner + "/" + repo);
+    }
 }
