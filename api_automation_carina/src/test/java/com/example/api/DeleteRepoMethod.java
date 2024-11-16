20df548ecf31ea3deae31bc4a494b39286c88f2f
Z_ <zxcvbbvvf2@gmail.com>
2024-11-16 09:02:11 -0600

    implemented DeleteRepoMethod class

--- api_automation_carina/src/test/java/com/example/api/DeleteRepoMethod.java
+++ api_automation_carina/src/test/java/com/example/api/DeleteRepoMethod.java
@@ -1,5 +1,12 @@
 package com.example.api;
 
-public class DeleteRepoMethod {
-    
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.utils.config.Configuration;
+
+public class DeleteRepoMethod extends AbstractApiMethodV2 {
+    public DeleteRepoMethod(String owner, String repo) {
+        super("api/delete_repo.json", null);
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        setMethodPath("/repos/" + owner + "/" + repo);
+    }
 }
