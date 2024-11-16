c0917157f63a58e96a3d386e5615a10c85112e96
Z_ <zxcvbbvvf2@gmail.com>
2024-11-16 08:49:03 -0600

    updated api jsons and implemented CreateRepoMethod class

--- api_automation_carina/src/test/java/com/example/api/CreateRepoMethod.java
+++ api_automation_carina/src/test/java/com/example/api/CreateRepoMethod.java
@@ -1,5 +1,11 @@
 package com.example.api;
 
-public class CreateRepoMethod {
-    
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.utils.config.Configuration;
+
+public class CreateRepoMethod extends AbstractApiMethodV2 {
+    public CreateRepoMethod() {
+        super("api/create_repo.json", null);
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+    }
 }
