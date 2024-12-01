07b1a75572e6bddf897389fad3f12c680a3ed327
Z_ <zxcvbbvvf2@gmail.com>
2024-12-01 09:44:11 -0600

    updated HomePage.java

--- carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/pages/HomePage.java
+++ carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/pages/HomePage.java
@@ -1,5 +1,35 @@
 package com.solvd.student.charles_borabon.carina_page_object_pattern.pages;
 
-public class HomePage {
-    
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends AbstractPage {
+    @FindBy(linkText="Sigh In")
+    private WebElement signInLink;
+
+    @FindBy(linkText="Create an Account")
+    private WebElement createAccountLink;
+
+    @FindBy(name="q")
+    private WebElement searchField;
+
+    @FindBy(xpath="//button[@title='Search']")
+    private WebElement searchButton;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        setPageAbsoluteURL("https://magento.softwaretestingboard.com/");
+    }
+
+    public LoginPage clickSignIn() {
+        signInLink.click();
+        return new LoginPage(getDriver());
+    }
+
+    public void search(String query) {
+        searchField.sendKeys(query);
+        searchButton.click();
+    }
 }
