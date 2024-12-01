953633c41528d9c608e17e3439291bb09fc0dc75
Z_ <zxcvbbvvf2@gmail.com>
2024-12-01 09:47:12 -0600

    updated LoginPage.java

--- carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/pages/LoginPage.java
+++ carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/pages/LoginPage.java
@@ -1,5 +1,28 @@
 package com.solvd.student.charles_borabon.carina_page_object_pattern.pages;
 
-public class LoginPage {
-    
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+
+public class LoginPage extends AbstractPage {
+    @FindBy(id="email")
+    private WebElement emailField;
+
+    @FindBy(id="pass")
+    private WebElement passwordField;
+
+    @FindBy(id="send2")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String email, String password) {
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
 }
