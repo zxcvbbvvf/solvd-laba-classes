c27a058bed6e179b8eb501faa660dcabaf53723a
Z_ <zxcvbbvvf2@gmail.com>
2024-12-01 11:34:55 -0600

    updated DriverManager

--- carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/utils/DriverManager.java
+++ carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/utils/DriverManager.java
@@ -1,5 +1,25 @@
 package com.solvd.student.charles_borabon.carina_page_object_pattern.utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverManager {
-    
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
