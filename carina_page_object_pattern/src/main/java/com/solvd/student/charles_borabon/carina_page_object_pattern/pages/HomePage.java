b97c47eed01b6bc6207222ce337635f67e34a20d
Z_ <zxcvbbvvf2@gmail.com>
2024-12-01 09:29:53 -0600

    created base files for new project

---
+++ carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/pages/HomePage.java
@@ -0,0 +1,5 @@
+package com.solvd.student.charles_borabon.carina_page_object_pattern.pages;
+
+public class HomePage {
+    
+}
---
+++ carina_page_object_pattern/src/main/java/com/solvd/student/charles_borabon/carina_page_object_pattern/tests/MagentoTests.java
@@ -0,0 +1,5 @@
+package com.solvd.student.charles_borabon.carina_page_object_pattern.tests;
+
+public class MagentoTests {
+    
+}
