e16db45c00ede19c43e659c640f27cf51e5d9bf4
Z_ <zxcvbbvvf2@gmail.com>
2024-12-09 11:10:48 -0600

    new base project

---
+++ android_automation/src/main/java/com/solvd/student/charles_borabon/android_automation/Android_automation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.solvd.student.charles_borabon.android_automation;
+
+/**
+ *
+ * @author Dustin_2
+ */
+public class Android_automation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
